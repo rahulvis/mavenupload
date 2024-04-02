bf002097caa6d92e20070ed72cc1b3e948ee8876
Rahul Vishwakarma <Rahul.vishwakarma@nsdlbank.co.in>
2024-04-02 16:07:34 +0530

    esign verification v7

--- mylibraryMaven/build.gradle.kts
+++ mylibraryMaven/build.gradle.kts
@@ -39,7 +39,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.example.mylibrarymaven"
             artifactId = "mylibrarymaven"
-            version = "0.0.1"
+            version = "0.0.2"
            // from(components["kotlin"])
             afterEvaluate {
                 from(components["release"])
